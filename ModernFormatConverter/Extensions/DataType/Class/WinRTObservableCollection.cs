254ab56f0667ab133b26a37febe7e4d2faeed4b5
怡飞 高 <363301617@qq.com>
2026-01-08 01:58:59 +0800

    Add WinRTObservableCollection
    Add WinRTObservableCollection

---
+++ ModernFormatConverter/Extensions/DataType/Class/WinRTObservableCollection.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace ModernFormatConverter.Extensions.DataType.Class
+{
+    /// <summary>
+    /// 扩展 ObservableCollection，以通知 WinUI 3 集合已经发生变化
+    /// </summary>
+    public class WinRTObservableCollection<T> : ObservableCollection<T>, Microsoft.UI.Xaml.Interop.INotifyCollectionChanged
+    {
+        public new event Microsoft.UI.Xaml.Interop.NotifyCollectionChangedEventHandler CollectionChanged;
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs args)
+        {
+            base.OnCollectionChanged(args);
+
+            switch (args.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    {
+                        CollectionChanged?.Invoke(this, new Microsoft.UI.Xaml.Interop.NotifyCollectionChangedEventArgs(Microsoft.UI.Xaml.Interop.NotifyCollectionChangedAction.Add, new BindableVector(args.NewItems), new BindableVector(args.OldItems), args.NewStartingIndex, args.OldStartingIndex));
+                        break;
+                    }
+                case NotifyCollectionChangedAction.Remove:
+                    {
+                        CollectionChanged?.Invoke(this, new Microsoft.UI.Xaml.Interop.NotifyCollectionChangedEventArgs(Microsoft.UI.Xaml.Interop.NotifyCollectionChangedAction.Remove, new BindableVector(args.NewItems), new BindableVector(args.OldItems), args.NewStartingIndex, args.OldStartingIndex));
+                        break;
+                    }
+                case NotifyCollectionChangedAction.Replace:
+                    {
+                        CollectionChanged?.Invoke(this, new Microsoft.UI.Xaml.Interop.NotifyCollectionChangedEventArgs(Microsoft.UI.Xaml.Interop.NotifyCollectionChangedAction.Replace, new BindableVector(args.NewItems), new BindableVector(args.OldItems), args.NewStartingIndex, args.OldStartingIndex));
+                        break;
+                    }
+                case NotifyCollectionChangedAction.Move:
+                    {
+                        CollectionChanged?.Invoke(this, new Microsoft.UI.Xaml.Interop.NotifyCollectionChangedEventArgs(Microsoft.UI.Xaml.Interop.NotifyCollectionChangedAction.Move, new BindableVector(args.NewItems), new BindableVector(args.OldItems), args.NewStartingIndex, args.OldStartingIndex));
+                        break;
+                    }
+                case NotifyCollectionChangedAction.Reset:
+                    {
+                        CollectionChanged?.Invoke(this, new Microsoft.UI.Xaml.Interop.NotifyCollectionChangedEventArgs(Microsoft.UI.Xaml.Interop.NotifyCollectionChangedAction.Reset, new BindableVector(args.NewItems), new BindableVector(args.OldItems), args.NewStartingIndex, args.OldStartingIndex));
+                        break;
+                    }
+            }
+        }
+    }
+}
