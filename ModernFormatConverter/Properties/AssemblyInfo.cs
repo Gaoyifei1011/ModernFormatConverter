52b857a4aa9426a9380b3fc07f2c6998dd36a078
怡飞 高 <363301617@qq.com>
2026-01-09 01:07:11 +0800

    Update CopyRight
    Update CopyRight

---
+++ ModernFormatConverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// 程序集信息设置
+[assembly: AssemblyCompany("高怡飞")]
+[assembly: AssemblyCopyright("Copyright 2026 高怡飞 版权所有")]
+[assembly: AssemblyDescription("格式转换器")]
+[assembly: AssemblyFileVersion("4.9.102.0")]
+[assembly: AssemblyInformationalVersion("4.9.102.0")]
+[assembly: AssemblyProduct("格式转换器")]
+[assembly: AssemblyTitle("格式转换器")]
+[assembly: AssemblyVersion("4.9.102.0")]
+
+// 应用程序默认区域性的资源控制器设置
+[assembly: NeutralResourcesLanguage("en-us")]
