b5469982a54d1481eac655c25fae3f92628fcc32
怡飞 高 <363301617@qq.com>
2026-01-14 01:33:52 +0800

    Add TaskManagerPage
    Add TaskManagerPage

---
+++ ModernFormatConverter/Views/Pages/TaskManagerPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace ModernFormatConverter.Views.Pages
+{
+    /// <summary>
+    /// 任务管理页面
+    /// </summary>
+    public sealed partial class TaskMangerPage : Page
+    {
+        public TaskMangerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
