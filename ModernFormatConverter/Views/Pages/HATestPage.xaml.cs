2b26a1a0e684cd3aa99445e0217664d750b87bc9
怡飞 高 <363301617@qq.com>
2026-01-14 01:34:03 +0800

    Add HATestPage
    Add HATestPage

---
+++ ModernFormatConverter/Views/Pages/HATestPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace ModernFormatConverter.Views.Pages
+{
+    /// <summary>
+    /// 硬件加速测试页面
+    /// </summary>
+    public sealed partial class HATestPage : Page
+    {
+        public HATestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
