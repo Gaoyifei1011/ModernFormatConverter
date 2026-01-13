cd58bf1a98e215b96444fd9f77ec96501661dff6
怡飞 高 <363301617@qq.com>
2026-01-14 01:34:16 +0800

    Add FileInformationPage
    Add FileInformationPage

---
+++ ModernFormatConverter/Views/Pages/FileInformationPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace ModernFormatConverter.Views.Pages
+{
+    /// <summary>
+    /// 文件信息界面
+    /// </summary>
+    public sealed partial class FileInformationPage : Page
+    {
+        public FileInformationPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
