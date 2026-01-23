b53643b13b7bcb1f81650d1e0c2621839621f1b0
怡飞 高 <363301617@qq.com>
2026-01-24 00:33:28 +0800

    Create IUIElementProtected.cs

---
+++ ModernFormatConverter/WindowsAPI/ComTypes/IUIElementProtected.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Input;
+using System;
+using System.Runtime.InteropServices;
+
+namespace ModernFormatConverter.WindowsAPI.ComTypes
+{
+    [ComImport, Guid("8F69B9E9-1F00-5834-9BF1-A9257BED39F0"), InterfaceType(ComInterfaceType.InterfaceIsIInspectable)]
+    public interface IUIElementProtected
+    {
+        InputCursor ProtectedCursor { get; set; }
+    }
+}
