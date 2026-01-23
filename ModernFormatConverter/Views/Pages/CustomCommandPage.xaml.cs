a8e30844a823184c300ea02f10ca6270529deec6
怡飞 高 <363301617@qq.com>
2026-01-24 00:33:50 +0800

    Finish develop CustomCommandPage
    Finish develop CustomCommandPage

--- ModernFormatConverter/Views/Pages/CustomCommandPage.xaml.cs
+++ ModernFormatConverter/Views/Pages/CustomCommandPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Input;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using ModernFormatConverter.Extensions.DataType.Enums;
+using ModernFormatConverter.Helpers.Controls;
+using ModernFormatConverter.Services.Root;
 using ModernFormatConverter.Views.NotificationTips;
 using ModernFormatConverter.Views.Windows;
 using System;
@@ -20,8 +23,12 @@ namespace ModernFormatConverter.Views.Pages
     /// </summary>
     public sealed partial class CustomCommandPage : Page, INotifyPropertyChanged
     {
+        private readonly string ProcessExitString = ResourceService.CustomCommandResource.GetString("ProcessExit");
         private readonly SynchronizationContext synchronizationContext = SynchronizationContext.Current;
         private readonly StringBuilder commandResultBuilder = new();
+        private ScrollViewer customCommandResultTextBoxScrollViewer;
+        private Process process;
+        private bool isLoaded;
 
         private bool _isRunning;
 
@@ -81,6 +88,26 @@ namespace ModernFormatConverter.Views.Pages
         #region 第一部分：自定义命令运行页面——挂载的事件
 
         /// <summary>
+        /// 加载完成后触发的事件
+        /// </summary>
+        private void OnLoaded(object sender, RoutedEventArgs args)
+        {
+            if (!isLoaded)
+            {
+                isLoaded = true;
+                (CommandResultTextBox as WindowsAPI.ComTypes.IUIElementProtected).ProtectedCursor = InputSystemCursor.Create(InputSystemCursorShape.Arrow);
+                if (XamlTreeHelper.FindDescendant<ScrollViewer>(CommandResultTextBox, "ContentElement") is ScrollViewer scrollViewer)
+                {
+                    customCommandResultTextBoxScrollViewer = scrollViewer;
+                }
+                if (XamlTreeHelper.FindDescendant<ScrollContentPresenter>(CommandResultTextBox, "ScrollContentPresenter") is ScrollContentPresenter scrollContentPresenter)
+                {
+                    (scrollContentPresenter.Content as WindowsAPI.ComTypes.IUIElementProtected).ProtectedCursor = InputSystemCursor.Create(InputSystemCursorShape.Arrow);
+                }
+            }
+        }
+
+        /// <summary>
         /// 运行命令
         /// </summary>
         private async void OnQuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
@@ -108,69 +135,172 @@ namespace ModernFormatConverter.Views.Pages
             {
                 await MainWindow.Current.ShowNotificationAsync(new OperationResultNotificationTip(OperationKind.CommandEmpty));
             }
+
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                commandResultBuilder.Clear();
+                CommandResultText = string.Empty;
+                customCommandResultTextBoxScrollViewer.ChangeView(null, 0, null, true);
+
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        process = new()
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "FFmpeg.exe",
+                                Arguments = string.Join(" ", "-nostdin", CommandText),
+                                RedirectStandardOutput = true,
+                                RedirectStandardError = true,
+                                UseShellExecute = false,
+                                CreateNoWindow = true,
+                                WorkingDirectory = Environment.CurrentDirectory,
+                                StandardOutputEncoding = Encoding.UTF8,
+                                StandardErrorEncoding = Encoding.UTF8
+                            },
+                            EnableRaisingEvents = true
+                        };
+                        process.OutputDataReceived += OnOutputDataReceived;
+                        process.ErrorDataReceived += OnErrorDataReceived;
+                        process.Exited += OnExited;
+                        process.Start();
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+                        process.WaitForExit();
+                        process.OutputDataReceived -= OnOutputDataReceived;
+                        process.ErrorDataReceived -= OnErrorDataReceived;
+                        process.Exited -= OnExited;
+                        process = null;
+                    }
+                    catch (Exception e)
+                    {
+                        LogService.WriteLog(TraceEventType.Error, nameof(ModernFormatConverter), nameof(CustomCommandPage), nameof(OnTestClicked), 1, e);
+                        process.OutputDataReceived -= OnOutputDataReceived;
+                        process.ErrorDataReceived -= OnErrorDataReceived;
+                        process.Exited -= OnExited;
+                        process.Dispose();
+                        process = null;
+                    }
+                });
+                IsRunning = false;
+            }
         }
 
         /// <summary>
         /// 停止运行命令
         /// </summary>
-        private void OnStopClicked(object sender, RoutedEventArgs args)
+        private async void OnStopClicked(object sender, RoutedEventArgs args)
         {
-            // TODO：未完成
+            if (IsRunning)
+            {
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        if (process is not null && !process.HasExited)
+                        {
+                            process.Kill();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        LogService.WriteLog(TraceEventType.Error, nameof(ModernFormatConverter), nameof(CustomCommandPage), nameof(OnStopClicked), 1, e);
+                    }
+                });
+
+                IsRunning = false;
+            }
         }
 
         /// <summary>
         /// 测试
         /// </summary>
-        private void OnTestClicked(object sender, RoutedEventArgs args)
+        private async void OnTestClicked(object sender, RoutedEventArgs args)
+        {
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                commandResultBuilder.Clear();
+                CommandResultText = string.Empty;
+                customCommandResultTextBoxScrollViewer.ChangeView(null, 0, null, true);
+
+                await Task.Run(() =>
+                {
+                    try
+                    {
+                        Process process = new()
+                        {
+                            EnableRaisingEvents = true,
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "FFmpeg.exe",
+                                Arguments = "-nostdin",
+                                RedirectStandardOutput = true,
+                                RedirectStandardError = true,
+                                UseShellExecute = false,
+                                CreateNoWindow = true,
+                                WorkingDirectory = Environment.CurrentDirectory,
+                                StandardOutputEncoding = Encoding.UTF8,
+                                StandardErrorEncoding = Encoding.UTF8
+                            },
+                        };
+                        process.OutputDataReceived += OnOutputDataReceived;
+                        process.ErrorDataReceived += OnErrorDataReceived;
+                        process.Exited += OnExited;
+                        process.Start();
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+                        process.WaitForExit();
+                        process.OutputDataReceived -= OnOutputDataReceived;
+                        process.ErrorDataReceived -= OnErrorDataReceived;
+                        process.Exited -= OnExited;
+                        process = null;
+                    }
+                    catch (Exception e)
+                    {
+                        LogService.WriteLog(TraceEventType.Error, nameof(ModernFormatConverter), nameof(CustomCommandPage), nameof(OnTestClicked), 1, e);
+                        process.OutputDataReceived -= OnOutputDataReceived;
+                        process.ErrorDataReceived -= OnErrorDataReceived;
+                        process.Exited -= OnExited;
+                        process.Dispose();
+                        process = null;
+                    }
+                });
+                IsRunning = false;
+            }
+        }
+
+        /// <summary>
+        /// 清空输出结果
+        /// </summary>
+        private void OnClearResultClicked(object sender, RoutedEventArgs args)
         {
             commandResultBuilder.Clear();
             CommandResultText = string.Empty;
-
-            Task.Run(() =>
-            {
-                try
-                {
-                    Process process = new()
-                    {
-                        StartInfo = new ProcessStartInfo
-                        {
-                            FileName = "FFmpeg.exe",
-                            RedirectStandardOutput = true,
-                            RedirectStandardError = true,
-                            UseShellExecute = false,
-                            CreateNoWindow = true,
-                            WorkingDirectory = Environment.CurrentDirectory,
-                            StandardOutputEncoding = Encoding.UTF8,
-                            StandardErrorEncoding = Encoding.UTF8
-                        }
-                    };
-
-                    process.OutputDataReceived += OnOutputDataReceived;
-                    process.ErrorDataReceived += OnErrorDataReceived;
-                    process.Start();
-                    process.BeginOutputReadLine();
-                    process.BeginErrorReadLine();
-                    process.WaitForExit();
-                    process.OutputDataReceived -= OnOutputDataReceived;
-                    process.ErrorDataReceived -= OnErrorDataReceived;
-                    process.Dispose();
-                }
-                catch (Exception e)
-                {
-                    Debug.WriteLine(e.Message);
-                }
-            });
+            customCommandResultTextBoxScrollViewer.ChangeView(null, 0, null, true);
         }
 
-        private void OnErrorDataReceived(object sender, DataReceivedEventArgs args)
+        /// <summary>
+        /// 命令输出结果内容发生变化时触发的事件
+        /// </summary>
+        private void OnCommandResultTextChanged(object sender, TextChangedEventArgs args)
         {
-            synchronizationContext.Post((_) =>
+            if (customCommandResultTextBoxScrollViewer.ComputedVerticalScrollBarVisibility is Visibility.Visible)
             {
-                commandResultBuilder.AppendLine(args.Data);
-                CommandResultText = commandResultBuilder.ToString();
-            }, null);
+                customCommandResultTextBoxScrollViewer.ChangeView(null, customCommandResultTextBoxScrollViewer.ExtentHeight, null, true);
+            }
         }
 
+        #endregion 第一部分：自定义命令运行页面——挂载的事件
+
+        #region 第二部分：自定义命令运行页面——自定义事件
+
+        /// <summary>
+        /// 当应用程序向其重定向 StandardOutput 流中写入行时发生的事件
+        /// </summary>
         private void OnOutputDataReceived(object sender, DataReceivedEventArgs args)
         {
             synchronizationContext.Post((_) =>
@@ -181,21 +311,38 @@ namespace ModernFormatConverter.Views.Pages
         }
 
         /// <summary>
-        /// 清空输出结果
+        /// 当应用程序写入其重定向 StandardError 流中时发生的事件
         /// </summary>
-        private void OnClearResultClicked(object sender, RoutedEventArgs args)
+        private void OnErrorDataReceived(object sender, DataReceivedEventArgs args)
         {
-            commandResultBuilder.Clear();
-            CommandResultText = string.Empty;
+            synchronizationContext.Post((_) =>
+            {
+                commandResultBuilder.AppendLine(args.Data);
+                CommandResultText = commandResultBuilder.ToString();
+            }, null);
         }
 
         /// <summary>
-        /// 命令输出结果内容发生变化时触发的事件
+        /// 在进程退出时发生的事件
         /// </summary>
-        private void OnCommandResultTextChanged(object sender, TextChangedEventArgs args)
+        private void OnExited(object sender, EventArgs args)
         {
+            Process process = sender as Process;
+            synchronizationContext.Post((_) =>
+            {
+                try
+                {
+                    commandResultBuilder.Append(string.Format(ProcessExitString, process.Id, "FFmpeg.exe", process.ExitCode, process.ExitCode));
+                    CommandResultText = commandResultBuilder.ToString();
+                    process.Dispose();
+                }
+                catch (Exception e)
+                {
+                    LogService.WriteLog(TraceEventType.Error, nameof(ModernFormatConverter), nameof(CustomCommandPage), nameof(OnExited), 1, e);
+                }
+            }, null);
         }
 
-        #endregion 第一部分：自定义命令运行页面——挂载的事件
+        #endregion 第二部分：自定义命令运行页面——自定义事件
     }
 }
