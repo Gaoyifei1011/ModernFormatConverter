fadf77325b829755e0bda26461180a402d48e8be
怡飞 高 <363301617@qq.com>
2026-02-03 01:16:04 +0800

    Update FileInformationPage UI implementation
    Update FileInformationPage UI implementation

---
+++ ModernFormatConverter/Extensions/DataType/Enums/FileInformationResultKind.cs
@@ -0,0 +1,13 @@
+﻿namespace ModernFormatConverter.Extensions.DataType.Enums
+{
+    public enum FileInformationResultKind
+    {
+        NotParse = 0,
+        Parsing = 1,
+        Failed = 2,
+        File = 3,
+        VideoFile = 4,
+        MusicFile = 5,
+        DocumentFile = 6,
+    }
+}
--- ModernFormatConverter/Views/Pages/FileInformationPage.xaml.cs
+++ ModernFormatConverter/Views/Pages/FileInformationPage.xaml.cs
@@ -1,15 +1,258 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
+using ModernFormatConverter.Extensions.DataType.Enums;
+using ModernFormatConverter.Services.Root;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+
+// 抑制 CA1822，IDE0060 警告
+#pragma warning disable CA1822,IDE0060
 
 namespace ModernFormatConverter.Views.Pages
 {
     /// <summary>
     /// 文件信息界面
     /// </summary>
-    public sealed partial class FileInformationPage : Page
+    public sealed partial class FileInformationPage : Page, INotifyPropertyChanged
     {
+        private readonly string DragOverContentString = ResourceService.FileInformationResource.GetString("DragOverContent");
+        private readonly string NoMultiFileString = ResourceService.FileInformationResource.GetString("NoMultiFile");
+        private readonly string ParsingFileInformationString = ResourceService.FileInformationResource.GetString("ParsingFileInformation");
+        private readonly string SelectFileString = ResourceService.FileInformationResource.GetString("SelectFile");
+
+        private FileInformationResultKind _fileInformationResultKind;
+
+        public FileInformationResultKind FileInformationResultKind
+        {
+            get { return _fileInformationResultKind; }
+
+            set
+            {
+                if (!Equals(_fileInformationResultKind, value))
+                {
+                    _fileInformationResultKind = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FileInformationResultKind)));
+                }
+            }
+        }
+
+        private string _fileInformationFailedContent;
+
+        public string FileInformationFailedContent
+        {
+            get { return _fileInformationFailedContent; }
+
+            set
+            {
+                if (!string.Equals(_fileInformationFailedContent, value))
+                {
+                    _fileInformationFailedContent = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FileInformationFailedContent)));
+                }
+            }
+        }
+
+        private ImageSource _fileThumbnailImage;
+
+        public ImageSource FileThumbnailImage
+        {
+            get { return _fileThumbnailImage; }
+
+            set
+            {
+                if (!Equals(_fileThumbnailImage, value))
+                {
+                    _fileThumbnailImage = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FileThumbnailImage)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public FileInformationPage()
         {
             InitializeComponent();
         }
+
+        #region 第一部分：重写父类事件
+
+        /// <summary>
+        /// 设置拖动的数据的可视表示形式
+        /// </summary>
+        protected override async void OnDragEnter(Microsoft.UI.Xaml.DragEventArgs args)
+        {
+            base.OnDragEnter(args);
+            DragOperationDeferral dragOperationDeferral = args.GetDeferral();
+
+            try
+            {
+                if (FileInformationResultKind is FileInformationResultKind.Parsing)
+                {
+                    args.AcceptedOperation = DataPackageOperation.None;
+                    args.DragUIOverride.IsCaptionVisible = true;
+                    args.DragUIOverride.IsContentVisible = false;
+                    args.DragUIOverride.IsGlyphVisible = true;
+                    args.DragUIOverride.Caption = ParsingFileInformationString;
+                }
+                else
+                {
+                    IReadOnlyList<IStorageItem> dragItemsList = await args.DataView.GetStorageItemsAsync();
+
+                    if (dragItemsList.Count is 1)
+                    {
+                        args.AcceptedOperation = DataPackageOperation.Copy;
+                        args.DragUIOverride.IsCaptionVisible = true;
+                        args.DragUIOverride.IsContentVisible = false;
+                        args.DragUIOverride.IsGlyphVisible = true;
+                        args.DragUIOverride.Caption = DragOverContentString;
+                    }
+                    else
+                    {
+                        args.AcceptedOperation = DataPackageOperation.None;
+                        args.DragUIOverride.IsCaptionVisible = true;
+                        args.DragUIOverride.IsContentVisible = false;
+                        args.DragUIOverride.IsGlyphVisible = true;
+                        args.DragUIOverride.Caption = NoMultiFileString;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogService.WriteLog(TraceEventType.Error, nameof(ModernFormatConverter), nameof(FileInformationPage), nameof(OnDragOver), 1, e);
+            }
+            finally
+            {
+                args.Handled = true;
+                dragOperationDeferral.Complete();
+            }
+        }
+
+        /// <summary>
+        /// 拖动文件完成后获取文件信息
+        /// </summary>
+        protected override async void OnDrop(Microsoft.UI.Xaml.DragEventArgs args)
+        {
+            DragOperationDeferral dragOperationDeferral = args.GetDeferral();
+            string filePath = string.Empty;
+            try
+            {
+                DataPackageView dataPackageView = args.DataView;
+                IReadOnlyList<IStorageItem> filesList = await Task.Run(async () =>
+                {
+                    try
+                    {
+                        if (dataPackageView.Contains(StandardDataFormats.StorageItems))
+                        {
+                            return await dataPackageView.GetStorageItemsAsync();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        LogService.WriteLog(TraceEventType.Error, nameof(ModernFormatConverter), nameof(FileInformationPage), nameof(OnDrop), 1, e);
+                    }
+
+                    return null;
+                });
+
+                if (filesList is not null && filesList.Count is 1)
+                {
+                    filePath = filesList[0].Path;
+                }
+            }
+            catch (Exception e)
+            {
+                LogService.WriteLog(TraceEventType.Error, nameof(ModernFormatConverter), nameof(FileInformationPage), nameof(OnDrop), 2, e);
+            }
+            finally
+            {
+                dragOperationDeferral.Complete();
+            }
+
+            if (File.Exists(filePath))
+            {
+                // TODO：未完成
+            }
+        }
+
+        #endregion 第一部分：重写父类事件
+
+        #region 第二部分：文件信息页面——挂载的事件
+
+        /// <summary>
+        /// 打开本地文件
+        /// </summary>
+        private void OnOpenFileClicked(object sender, RoutedEventArgs args)
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                Multiselect = false,
+                Title = SelectFileString
+            };
+            if (openFileDialog.ShowDialog() is DialogResult.OK && !string.IsNullOrEmpty(openFileDialog.FileName))
+            {
+                // TODO：未完成
+            }
+            openFileDialog.Dispose();
+        }
+
+        /// <summary>
+        /// 打开文件属性页面
+        /// </summary>
+        private void OnFilePropertiesClicked(object sender, RoutedEventArgs args)
+        {
+            // TODO：未完成
+        }
+
+        /// <summary>
+        /// 文件定位
+        /// </summary>
+        private void OnOpenFileLocationClicked(object sender, RoutedEventArgs args)
+        {
+            // TODO：未完成
+        }
+
+        /// <summary>
+        /// 复制到剪贴板
+        /// </summary>
+        private void OnCopyClipboardClicked(object sender, RoutedEventArgs args)
+        {
+            // TODO：未完成
+        }
+
+        /// <summary>
+        /// 获取文件信息解析是否成功
+        /// </summary>
+        private Visibility GetFileInformationSuccessfullyState(FileInformationResultKind fileInformationResultKind, bool isSuccessfully)
+        {
+            return isSuccessfully ? (fileInformationResultKind is FileInformationResultKind.File || fileInformationResultKind is FileInformationResultKind.VideoFile || fileInformationResultKind is FileInformationResultKind.MusicFile || fileInformationResultKind is FileInformationResultKind.DocumentFile) ? Visibility.Visible : Visibility.Collapsed : (fileInformationResultKind is FileInformationResultKind.File || fileInformationResultKind is FileInformationResultKind.VideoFile || fileInformationResultKind is FileInformationResultKind.MusicFile || fileInformationResultKind is FileInformationResultKind.DocumentFile) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        /// <summary>
+        /// 检查文件信息解析是否成功
+        /// </summary>
+        private Visibility CheckFileInformationState(FileInformationResultKind fileInformationResultKind, FileInformationResultKind comparedFileInformationResultKind)
+        {
+            return Equals(fileInformationResultKind, comparedFileInformationResultKind) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        /// <summary>
+        /// 获取是否正在解析中
+        /// </summary>
+        private bool GetIsParsing(FileInformationResultKind fileInformationReusltKind)
+        {
+            return fileInformationReusltKind is not FileInformationResultKind.Parsing;
+        }
+
+        #endregion 第二部分：文件信息页面——挂载的事件
     }
 }
