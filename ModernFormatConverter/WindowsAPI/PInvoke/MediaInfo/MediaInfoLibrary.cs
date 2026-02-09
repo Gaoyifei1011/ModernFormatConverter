51aabd47bbc4f22ea660c4b4d556f203e977a789
怡飞 高 <363301617@qq.com>
2026-02-10 01:10:53 +0800

    Import MediaInfoLibrary API
    Import MediaInfoLibrary API

---
+++ ModernFormatConverter/WindowsAPI/PInvoke/MediaInfo/MediaInfoLibrary.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+#pragma warning disable CA1401
+
+namespace ModernFormatConverter.WindowsAPI.PInvoke.MediaInfo
+{
+    /// <summary>
+    /// MediaInfo.dll 函数库
+    /// </summary>
+    public static class MediaInfoLibrary
+    {
+        private const string MediaInfo = "MediaInfo.dll";
+
+        [DllImport(MediaInfo, CharSet = CharSet.Unicode, EntryPoint = "MediaInfo_Close", PreserveSig = true, SetLastError = false)]
+        public static extern void MediaInfo_Close(nint handle);
+
+        [DllImport(MediaInfo, CharSet = CharSet.Unicode, EntryPoint = "MediaInfo_Delete", PreserveSig = true, SetLastError = false)]
+        public static extern void MediaInfo_Delete(nint handle);
+
+        [DllImport(MediaInfo, CharSet = CharSet.Unicode, EntryPoint = "MediaInfo_Inform", PreserveSig = true, SetLastError = false)]
+        public static extern nint MediaInfo_Inform(nint handle, int reserved);
+
+        [DllImport(MediaInfo, CharSet = CharSet.Unicode, EntryPoint = "MediaInfo_New", PreserveSig = true, SetLastError = false)]
+        public static extern nint MediaInfo_New();
+
+        [DllImport(MediaInfo, CharSet = CharSet.Unicode, EntryPoint = "MediaInfo_Open", PreserveSig = true, SetLastError = false)]
+        public static extern int MediaInfo_Open(nint handle, [MarshalAs(UnmanagedType.LPWStr)] string fileName);
+
+        [DllImport(MediaInfo, CharSet = CharSet.Unicode, EntryPoint = "MediaInfo_Option", PreserveSig = true, SetLastError = false)]
+        public static extern nint MediaInfo_Option(nint handle, [MarshalAs(UnmanagedType.LPWStr)] string option, [MarshalAs(UnmanagedType.LPWStr)] string value);
+    }
+}
