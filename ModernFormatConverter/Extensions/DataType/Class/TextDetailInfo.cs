c6bde092a288008f8026c693f43d099935c6a151
怡飞 高 <363301617@qq.com>
2026-03-17 01:08:28 +0800

    Update TextDetailInfo
    Update TextDetailInfo

--- ModernFormatConverter/Extensions/DataType/Class/TextDetailInfo.cs
+++ ModernFormatConverter/Extensions/DataType/Class/TextDetailInfo.cs
@@ -5,5 +5,22 @@
     /// </summary>
     public class TextDetailInfo
     {
+        public string ID { get; set; }
+
+        public string Format { get; set; }
+
+        public string CodecID { get; set; }
+
+        public string CodecIDInfo { get; set; }
+
+        public string Duration { get; set; }
+
+        public string BitRate { get; set; }
+
+        public string CountOfElements { get; set; }
+
+        public string StreamSize { get; set; }
+
+        public string Default { get; set; }
     }
 }
