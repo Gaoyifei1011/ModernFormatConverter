0f058dcadee22ebfb1f9d5e3dbd98f0d1a52c33e
怡飞 高 <363301617@qq.com>
2026-03-17 00:24:13 +0800

    Update VideoDetailInfo
    Update VideoDetailInfo

--- ModernFormatConverter/Extensions/DataType/Class/VideoDetailInfo.cs
+++ ModernFormatConverter/Extensions/DataType/Class/VideoDetailInfo.cs
@@ -5,22 +5,60 @@
     /// </summary>
     public class VideoDetailInfo
     {
+        public string ID { get; }
+
         public string Format { get; set; }
+
+        public string FormatInfo { get; set; }
 
         public string FormatProfile { get; set; }
 
         public string CodecID { get; set; }
 
+        public string CodecIDInfo { get; set; }
+
         public string Duration { get; set; }
 
-        public string OverallBitRate { get; set; }
+        public string SourceDuration { get; set; }
 
-        public string MovieName { get; set; }
+        public string BitRate { get; set; }
 
-        public string WritingApplication { get; set; }
+        public string Width { get; set; }
 
-        public string Copyright { get; set; }
+        public string Height { get; set; }
 
-        public string Comment { get; set; }
+        public string DisplayAspectRatio { get; set; }
+
+        public string FrameRate { get; set; }
+
+        public string FrameRateMode { get; set; }
+
+        public string MinimumFrameRate { get; set; }
+
+        public string MaximumFrameRate { get; set; }
+
+        public string ColorSpace { get; set; }
+
+        public string ChromaSubsampling { get; set; }
+
+        public string BitDepth { get; set; }
+
+        public string BitsPixelFrame { get; set; }
+
+        public string StreamSize { get; set; }
+
+        public string SourceStreamSize { get; set; }
+
+        public string WritingLibrary { get; set; }
+
+        public string ColorRange { get; set; }
+
+        public string ColorPrimaries { get; set; }
+
+        public string TransferCharacteristics { get; set; }
+
+        public string MatrixCoefficients { get; set; }
+
+        public string CodecConfigurationBox { get; set; }
     }
 }
