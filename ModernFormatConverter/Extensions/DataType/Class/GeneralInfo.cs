ec010b3aa04109720e910dfdd7aad21f7482bbda
怡飞 高 <363301617@qq.com>
2026-03-21 02:00:30 +0800

    Update GeneralInfo
    Update GeneralInfo

--- ModernFormatConverter/Extensions/DataType/Class/GeneralInfo.cs
+++ ModernFormatConverter/Extensions/DataType/Class/GeneralInfo.cs
@@ -5,5 +5,44 @@
     /// </summary>
     public class GeneralInfo
     {
+        public string CompleteName { get; set; }
+
+        public string Format { get; set; }
+
+        public string FormatVersion { get; set; }
+
+        public string FormatProfile { get; set; }
+
+        public string CodecID { get; set; }
+
+        public string FileSize { get; set; }
+
+        public string UniqueID { get; set; }
+
+        public string EncodedDate { get; set; }
+
+        public string Duration { get; set; }
+
+        public string OverallBitRate { get; set; }
+
+        public string FrameRate { get; set; }
+
+        public string StreamSize { get; set; }
+
+        public string RecordedDate { get; set; }
+
+        public string WritingApplication { get; set; }
+
+        public string WritingLibrary { get; set; }
+
+        #region 音频专用信息
+
+        public string Album { get; set; }
+
+        public string TrackName { get; set; }
+
+        public string Performer { get; set; }
+
+        #endregion 音频专用信息
     }
 }
