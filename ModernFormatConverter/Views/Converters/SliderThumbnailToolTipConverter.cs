d1abff94fa6cc39ca54a1017d7a3aebb448efa17
怡飞 高 <363301617@qq.com>
2026-05-26 01:24:58 +0800

    Add SliderThumbnailToolTipConverter
    Add SliderThumbnailToolTipConverter

---
+++ ModernFormatConverter/Views/Converters/SliderThumbnailToolTipConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+// 抑制 CA1806 警告
+#pragma warning disable CA1806
+
+namespace ModernFormatConverter.Views.Converters
+{
+    /// <summary>
+    /// 滑动条数字格式化显示
+    /// </summary>
+    public class SliderThumbnailToolTipConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value is double doubleValue ? doubleValue.ToString("0.####") : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
