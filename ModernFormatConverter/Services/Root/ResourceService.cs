abf261ebd6205f6aac90886eab9cedddf2e86cc1
怡飞 高 <363301617@qq.com>
2026-05-29 18:14:04 +0800

    Update ResourceService.cs

--- ModernFormatConverter/Services/Root/ResourceService.cs
+++ ModernFormatConverter/Services/Root/ResourceService.cs
@@ -58,6 +58,8 @@ namespace ModernFormatConverter.Services.Root
 
         public static ResourceManager VideoConversionOutputConfigurationResource { get; } = new("ModernFormatConverter.Strings.VideoConversionOutputConfiguration", CurrentAssembly);
 
+        public static ResourceManager VideoListResource { get; } = new("ModernFormatConverter.Strings.VideoList", CurrentAssembly);
+
         public static ResourceManager VideoExportPictureOutputConfigurationResource { get; } = new("ModernFormatConverter.Strings.VideoExportPictureOutputConfiguration", CurrentAssembly);
 
         public static ResourceManager WindowResource { get; } = new("ModernFormatConverter.Strings.Window", CurrentAssembly);
