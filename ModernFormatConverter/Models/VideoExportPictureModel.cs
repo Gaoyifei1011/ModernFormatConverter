3b2a8d470c816f45da619503a105094f7ad97382
怡飞 高 <363301617@qq.com>
2026-05-31 01:07:14 +0800

    Add VideoExportPicture data models
    Add VideoExportPicture data models

---
+++ ModernFormatConverter/Models/VideoExportPictureModel.cs
@@ -0,0 +1,15 @@
+﻿using ModernFormatConverter.Extensions.DataType.Class;
+
+namespace ModernFormatConverter.Models
+{
+    /// <summary>
+    /// 视频导出图片数据模型
+    /// </summary>
+    public class VideoExportPictureModel
+    {
+        /// <summary>
+        /// 视频导出图片列表
+        /// </summary>
+        public WinRTObservableCollection<VideoExportPictureFileModel> VideoExportPictureCollection { get; } = [];
+    }
+}
