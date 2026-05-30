f2c77fbe55f13b013e7256add583fa5740e3671a
怡飞 高 <363301617@qq.com>
2026-05-31 01:08:04 +0800

    Add VideoConcat data models
    Add VideoConcat data models

---
+++ ModernFormatConverter/Models/VideoConcatFileModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ModernFormatConverter.Models
+{
+    /// <summary>
+    /// 视频合并文件数据模型
+    /// </summary>
+    public class VideoConcatFileModel
+    {
+        /// <summary>
+        /// 文件名称
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public long FileSize { get; set; }
+
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public string FileSizeString { get; set; }
+
+        /// <summary>
+        /// 视频持续时间
+        /// </summary>
+        public TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// 视频持续时间
+        /// </summary>
+        public string DurationString { get; set; }
+
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// 屏幕高度
+        /// </summary>
+        public string ScreenSizeHeight { get; set; }
+
+        /// <summary>
+        /// 屏幕宽度
+        /// </summary>
+        public string ScreenSizeWidth { get; set; }
+    }
+}
---
+++ ModernFormatConverter/Models/VideoConcatModel.cs
@@ -0,0 +1,17 @@
+﻿using ModernFormatConverter.Extensions.DataType.Class;
+
+namespace ModernFormatConverter.Models
+{
+    /// <summary>
+    /// 视频合并数据模型
+    /// </summary>
+    public class VideoConcatModel
+    {
+        public WinRTObservableCollection<VideoConcatFileModel> VideoConcatFileCollection { get; } = [];
+
+        /// <summary>
+        /// 视频转换输出配置
+        /// </summary>
+        public VideoConversionOutputConfigurationModel VideoConversionOutputConfiguration { get; set; }
+    }
+}
