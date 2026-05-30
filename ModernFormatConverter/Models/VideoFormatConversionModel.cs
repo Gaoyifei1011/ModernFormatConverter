af3b0417322b4b6ed4ecb4d901c1192d35fda70b
怡飞 高 <363301617@qq.com>
2026-05-31 01:07:31 +0800

    Add VideoFormatConversion data models
    Add VideoFormatConversion data models

---
+++ ModernFormatConverter/Models/VideoFormatConversionModel.cs
@@ -0,0 +1,12 @@
+﻿using ModernFormatConverter.Extensions.DataType.Class;
+
+namespace ModernFormatConverter.Models
+{
+    /// <summary>
+    /// 视频格式转换数据模型
+    /// </summary>
+    public class VideoFormatConversionModel
+    {
+        public WinRTObservableCollection<VideoFormatConversionFileModel> VideoFormatConversionFileCollection { get; } = [];
+    }
+}
