327e926f3d241537c06480860e079c1e707eeab9
怡飞 高 <363301617@qq.com>
2026-06-01 00:43:51 +0800

    Add VideoConversionNavigationParameterModel
    Add VideoConversionNavigationParameterModel

---
+++ ModernFormatConverter/Models/VideoConversionNavigationParameterModel.cs
@@ -0,0 +1,25 @@
+﻿using ModernFormatConverter.Extensions.DataType.Enums;
+
+namespace ModernFormatConverter.Models
+{
+    /// <summary>
+    /// 视频转换导航参数数据模型
+    /// </summary>
+    public class VideoConversionNavigationParameterModel
+    {
+        /// <summary>
+        /// 视频转换类型
+        /// </summary>
+        public VideoConversionTypeKind VideoConversionTypeKind { get; set; }
+
+        /// <summary>
+        /// 是否修改全部数据
+        /// </summary>
+        public bool IsGlobalSettings { get; set; }
+
+        /// <summary>
+        /// 视频转换数据
+        /// </summary>
+        public object VideoConversionData { get; set; }
+    }
+}
