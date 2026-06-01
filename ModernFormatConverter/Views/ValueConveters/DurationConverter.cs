1a7c666e95108c250b6688ff0968c33d1011c24d
怡飞 高 <363301617@qq.com>
2026-06-02 01:08:53 +0800

    Add DurationConverter and FizeSizeConverter
    Add DurationConverter and FizeSizeConverter

---
+++ ModernFormatConverter/Views/ValueConveters/DurationConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+// 抑制 CA1806 警告
+#pragma warning disable CA1806
+
+namespace ModernFormatConverter.Views.ValueConveters
+{
+    /// <summary>
+    /// 时间值转换器
+    /// </summary>
+    public class DurationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value is TimeSpan timeSpan ? Equals(value, TimeSpan.Zero) ? "00:00:00"   : string.Format(@"{0:00}:{1:00}:{2:00}", Math.Truncate(timeSpan.TotalHours), timeSpan.Minutes, timeSpan.Minutes): "N/A";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
---
+++ ModernFormatConverter/Views/ValueConveters/FileSizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Data;
+using ModernFormatConverter.Helpers.Root;
+using System;
+
+// 抑制 CA1806 警告
+#pragma warning disable CA1806
+
+namespace ModernFormatConverter.Views.ValueConveters
+{
+    /// <summary>
+    /// 文件大小值转换器
+    /// </summary>
+    public class FileSizeValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value is long fileSize ? VolumeSizeHelper.ConvertVolumeSizeToString(fileSize) : "N/A";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
