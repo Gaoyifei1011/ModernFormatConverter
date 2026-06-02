a497a437cba5d014e5543a6dc5ec07fd00b60efd
怡飞 高 <363301617@qq.com>
2026-06-02 10:24:11 +0800

    Add AudioConversionNavigationParameter
    Add AudioConversionNavigationParameter

---
+++ ModernFormatConverter/Extensions/DataType/Class/AudioConversionNavigationParameter.cs
@@ -0,0 +1,25 @@
+﻿using ModernFormatConverter.Extensions.DataType.Enums;
+
+namespace ModernFormatConverter.Extensions.DataType.Class
+{
+    /// <summary>
+    /// 音频转换导航参数
+    /// </summary>
+    public class AudioConversionNavigationParameter
+    {
+        /// <summary>
+        /// 音频转换类型
+        /// </summary>
+        public AudioConversionTypeKind AudioConversionTypeKind { get; set; }
+
+        /// <summary>
+        /// 是否修改全部数据
+        /// </summary>
+        public bool IsGlobalSettings { get; set; }
+
+        /// <summary>
+        /// 音频转换数据
+        /// </summary>
+        public object AudioConversionData { get; set; }
+    }
+}
