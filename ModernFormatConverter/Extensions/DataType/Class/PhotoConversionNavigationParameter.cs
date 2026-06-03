054503f3621e35cde89ecc063ea5a156ff165046
怡飞 高 <363301617@qq.com>
2026-06-03 16:05:11 +0800

    Add PhotoConversionNavigationParameter
    Add PhotoConversionNavigationParameter

---
+++ ModernFormatConverter/Extensions/DataType/Class/PhotoConversionNavigationParameter.cs
@@ -0,0 +1,25 @@
+﻿using ModernFormatConverter.Extensions.DataType.Enums;
+
+namespace ModernFormatConverter.Extensions.DataType.Class
+{
+    /// <summary>
+    /// 图片转换导航参数
+    /// </summary>
+    public class PhotoConversionNavigationParameter
+    {
+        /// <summary>
+        /// 图片转换类型
+        /// </summary>
+        public PhotoConversionTypeKind PhotoConversionTypeKind { get; set; }
+
+        /// <summary>
+        /// 是否修改全部数据
+        /// </summary>
+        public bool IsGlobalSettings { get; set; }
+
+        /// <summary>
+        /// 图片转换数据
+        /// </summary>
+        public object PhotoConversionData { get; set; }
+    }
+}
