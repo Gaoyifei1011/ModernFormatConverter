a075054570c5f78cc687cf6371cb59c41b8b2a80
怡飞 高 <363301617@qq.com>
2026-08-10 00:31:11 +0800

    Fix window icon empty bug
    Fix window icon empty bug

--- ModernFormatConverter/MainApp.xaml.cs
+++ ModernFormatConverter/MainApp.xaml.cs
@@ -17,6 +17,7 @@ namespace ModernFormatConverter
     /// </summary>
     public partial class MainApp : Application, IDisposable
     {
+        private Icon windowIcon;
         private bool isDisposed;
 
         public Window MainWindow { get; private set; }
@@ -55,11 +56,11 @@ namespace ModernFormatConverter
         {
             try
             {
-                Icon icon = Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+                windowIcon = Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
 
-                if (icon is not null)
+                if (windowIcon is not null)
                 {
-                    appWindow.SetIcon(new IconId() { Value = (ulong)icon.Handle });
+                    appWindow.SetIcon(new IconId() { Value = (ulong)windowIcon.Handle });
                 }
             }
             catch (Exception e)
